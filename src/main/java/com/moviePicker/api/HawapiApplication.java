949651af4c1c9a5d78a700f7a4d6c2be98f1b75f
hcpak <jasotn12@naver.com>
2021-07-26 19:14:47 +0900

    Feat: Spring boot 초기 설정

---
+++ src/main/java/com/moviePicker/api/HawapiApplication.java
@@ -0,0 +1,13 @@
+package com.moviePicker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HawapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviePicker/api/HawapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviePicker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
