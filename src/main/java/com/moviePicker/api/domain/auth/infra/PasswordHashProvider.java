7971e393080889f95240c5cba620e0f82f17a46d
hcpak <jasotn12@naver.com>
2021-08-03 21:05:42 +0900

    feat: auth infra 추가

---
+++ src/main/java/com/moviePicker/api/domain/auth/infra/PasswordHashProvider.java
@@ -0,0 +1,20 @@
+package com.moviePicker.api.domain.auth.infra;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PasswordHashProvider {
+    private final PasswordEncoder encoder;
+
+    public String encodePassword(String rawPassword) {
+        return encoder.encode(rawPassword);
+    }
+
+    public boolean matches(String rawPassword, String hashPassword) {
+        return encoder.matches(rawPassword, hashPassword);
+    }
+}
+
